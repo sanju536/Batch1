dab6d337477728241b16e8621ef8fae12b643193
sanju536 <sunnysanju532@gmail.com>
2025-01-21 19:14:31 +0530

    Username,Password And String

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String username = "harshasree";
+        String password = "qwertyuiop";
+        while ((username == "harshasree") && (password == "qwertyuiop")) {
+            System.out.println("welcome to Malla Reddy University!!");
+            break;
+        }
+    }
+}
