85e4f94d64a79b05928c75e084d2c5907bd19156
sanju536 <sunnysanju532@gmail.com>
2025-01-21 19:10:55 +0530

    Greater Number

---
+++ Demo002.java
@@ -0,0 +1,11 @@
+public class Demo002 {
+    public static void main(String[] args){
+        int num1=23;
+        int num2=34;
+        if(num1>num2){
+            System.out.println(num1 +" is greater than "+num2);
+        }else{
+            System.out.println(num2+" is greater than "+ num1);
+        }
+    }
+}
