935cb692134808acc6f02a795b4da4e39452b0d0
sanju536 <sunnysanju532@gmail.com>
2025-01-21 19:13:49 +0530

    Username and Password

---
+++ Demo005.java
@@ -0,0 +1,10 @@
+public class Demo005 {
+    public static void main(String[] args) {
+        String username="harsha";
+        String password="qwertyuiop";
+        while((username == "harsha") && (password == "qwertyuiop")){
+            System.out.println("Welcome user!!");
+            break;
+        }
+    }
+}
