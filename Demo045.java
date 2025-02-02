acb2840cd6397606fad45d3aeddd01925ebb4764
sanju536 <sunnysanju532@gmail.com>
2025-02-02 21:14:52 +0530

    Values After & Before Swapping

---
+++ Demo045.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Demo045 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value [a]:");
+        int a = sc.nextInt();
+        System.out.println("Enter value [b]:");
+        int b = sc.nextInt();
+        System.out.println("Value of a before swapping is " + a + " Value of b before swapping is " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("Value of a after swapping is " + a + " Value of b after swapping is " + b);
+        sc.close();
+    }
+}
