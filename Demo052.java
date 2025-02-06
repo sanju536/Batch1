c099a1679bc1a552bb8d631cd64beee3ee4300a9
sanju536 <sunnysanju532@gmail.com>
2025-02-06 10:48:56 +0530

    Program To Check Number Is Armstrong Or not

---
+++ Demo052.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Demo052 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int sum = 0;
+        int numOfDigits = String.valueOf(number).length();
+        while (number != 0) {
+            int digit = number % 10; 
+            sum += Math.pow(digit, numOfDigits);  
+            number /= 10;  
+        }
+        if (sum == originalNumber) {
+            System.out.println(originalNumber + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNumber + " is not an Armstrong number.");
+        }
+        scanner.close(); 
+    }
+}
