26094cd935760d96c4b2334021b68e003c1c98ad
sanju536 <sunnysanju532@gmail.com>
2025-02-06 10:51:46 +0530

    Program To Check Number Is Strong Or Not

---
+++ Demo053.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Demo053 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int sum = 0;
+        while (number != 0) {
+            int digit = number % 10;  
+            sum += factorial(digit);  
+            number /= 10;  
+        }
+        if (sum == originalNumber) {
+            System.out.println(originalNumber + " is a Strong number.");
+        } else {
+            System.out.println(originalNumber + " is not a Strong number.");
+        }
+        scanner.close(); 
+    }
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
