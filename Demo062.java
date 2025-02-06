52df0bb5faa696b198157261eaa9992f8ec9ff79
sanju536 <sunnysanju532@gmail.com>
2025-02-06 11:05:02 +0530

    To Print 1st,2nd And Last Element Of An Array

---
+++ Demo062.java
@@ -0,0 +1,12 @@
+public class Demo062 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        if (arr.length >= 2) {
+            System.out.println("1st Element: " + arr[0]);
+            System.out.println("2nd Element: " + arr[1]);
+            System.out.println("Last Element: " + arr[arr.length - 1]);
+        } else {
+            System.out.println("Array does not have enough elements.");
+        }
+    }
+}
